ffd93aca7a0bb53cfd27bca03574fc8395f756d6
Split174 <sergei.popov174@gmail.com>
2025-12-23 03:59:35 +0500

    fix vendor hash

--- pkgs/secure-keepass-fuse/default.nix
+++ pkgs/secure-keepass-fuse/default.nix
@@ -14,7 +14,7 @@ buildGoModule rec {
     hash = "sha256-jtBDamEp7lL1BXhHCCgQb873g+vuDAKZtn5PdfZ4WpY=";
   };
 
-  vendorHash = "sha256-83uE2xirQ1R/hiRoIxLo4YVCJ6mybfDePK9PCinyJPM=";
+  vendorHash = "sha256-+4Syd4z5SN4pe8lWTXeSN9NVTSFRY+XOns8F2leEGOo=";
 
   ldflags = ["-s" "-w"];
 
