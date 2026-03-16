df06b66af1251479a069c6064d60ad09f7b48a9d
Sergey Popov <sergei.popov174@gmail.com>
2026-03-16 19:02:58 +0500

    test new version secure keepass fuse

--- pkgs/secure-keepass-fuse/default.nix
+++ pkgs/secure-keepass-fuse/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "secure-keepass-fuse";
-  version = "0.0.1";
+  version = "feature/move-to-interactive-window";
 
   src = fetchFromGitHub {
     owner = "Split174";
     repo = "secure-keepass-fuse";
     rev = "v${version}";
-    hash = "sha256-jtBDamEp7lL1BXhHCCgQb873g+vuDAKZtn5PdfZ4WpY=";
+    hash = "sha256-qEioTkuSBa/R5qg3R/0STO17r+BZDJbkWjS1GYOYIYI=";
   };
 
   vendorHash = "sha256-+4Syd4z5SN4pe8lWTXeSN9NVTSFRY+XOns8F2leEGOo=";
