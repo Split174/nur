5e40ca86edbfa144b3aebd6d57438f0c94eb9805
Split174 <sergei.popov174@gmail.com>
2026-04-18 21:32:04 +0500

    ап версии

--- pkgs/ygg-manager/default.nix
+++ pkgs/ygg-manager/default.nix
@@ -5,21 +5,21 @@
 }:
 buildGoModule rec {
   pname = "ygg-manager";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "Split174";
     repo = "ygg-manager";
     rev = "v${version}";
-    hash = "sha256-8YxYgvKRznIgvBDbKmoEj0phhiCNQrU7l88fDq9vCso=";
+    hash = "sha256-szHq3pXathoxnUUlcNU1LvybUrxdzjJb7cWqFsanZVg=";
   };
 
-  vendorHash = null;
+  vendorHash = "sha256-Z8V1a3uJdG/lj6AP4Xly01MQSq/yBnB2/TuERrrj0o0=";
 
   ldflags = ["-s" "-w"];
 
   meta = {
-    description = "";
+    description = "Automated Yggdrasil peer manager written in Go. Keeps your node healthy by removing slow/unstable peers and discovering the best public nodes with country-based filtering and latency checks";
     homepage = "https://github.com/Split174/ygg-manager";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; ["Split174"];
